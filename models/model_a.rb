d8ab239135cbce3f59fe195e4c85b69e89ef882a
James Harkins <jh.harkins@protonmail.com>
2021-12-16 17:17:12 -0700

    Add dinosaur model

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+dinosaur = {
+  "genus" => "Tyrannosaurus",
+  "species" => "Rex",
+  "num_legs" => 2,
+  "num_arms" => 2,
+  "has_wings" => false,
+  "carnivorous" => true,
+  "dietary_examples" => ["Edmontosaurus", "Triceratops", "Tyrannosaurus Rex"],
+  "movies_featuring_t_rex" => ["Jurassic Park", "The Land Before Time", "Night At The Museum"],
+}

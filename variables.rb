70e99e47881699cecc30e77ec559298cc7306359
James Harkins <jh.harkins@protonmail.com>
2021-12-16 17:00:21 -0700

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+movie = "The Big Lebowski"
+is_entertaining = true
+movie = "There Will Be Blood"
+num_4 = 5
+num_6 = 10
+product = num_6 * num_4
+favorite_foods = ["Beef Wellington", "Bolognese", "Jambalaya"]
+favorite_foods.shift
+puts movie.length
